8aaa3ba7bf5a0940b27f058c0747544807f59d37
Hack Reactor Students <accounts@hackreactor.com>
2015-11-04 15:41:32 -0800

    set environment variables, injected dependencies

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var grunt = require('./Gruntfile.js');
 var app = require('./server-config.js');
 
-var port = 4568;
-
+// var port = 4568;
+var port = process.env['PORT'] || 4568;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
